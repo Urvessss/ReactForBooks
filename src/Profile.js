0627b5164e5e1f2f732f02f333743657a744ef93
Urvesh <90395969+Urvessss@users.noreply.github.com>
2023-02-10 10:51:13 +0530

    first

---
+++ src/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = ({title}) => {
+  return (
+    <div>
+        <ul>
+            <li>We learn {title} Cloud</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Profile
